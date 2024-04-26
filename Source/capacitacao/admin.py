0679c8cbfb3129877dcad06b7c3bfd70eb42bc67
Gabriel Macaúbas <gabrielmacaubas@outlook.com>
2024-04-26 17:37:22 -0300

    feat: notas e sub soft skills

--- Source/capacitacao/admin.py
+++ Source/capacitacao/admin.py
@@ -2,7 +2,8 @@ from django.contrib import admin
 
 from .models import (
     Mentor, Discente, Projeto,
-    DiscenteProjeto, Autoavaliacao, SoftSkill)
+    DiscenteProjeto, Autoavaliacao, SoftSkill,
+    AutoavaliacaoNota, SubSoftSkill)
 
 admin.site.register(Mentor)
 admin.site.register(Discente)
@@ -10,3 +11,5 @@ admin.site.register(Projeto)
 admin.site.register(DiscenteProjeto)
 admin.site.register(Autoavaliacao)
 admin.site.register(SoftSkill)
+admin.site.register(SubSoftSkill)
+admin.site.register(AutoavaliacaoNota)
--- Source/capacitacao/models/AutoavaliacaoNota.py
+++
@@ -1,6 +0,0 @@
-from django.db import models
-
-
-class AutoavaliacaoNota(models.Model):
-    # campos
-    nota = models.DecimalField(max_digits=8, decimal_places=2, null=True, default=0)
